27fab0401fe3a1c6d9debb37e56f96cfc5240935
b0ink <40929320+b0ink@users.noreply.github.com>
2024-04-04 06:07:35 +1100

    Long press to delete tasks with confirmatiom dialog

--- app/src/main/java/com/example/taskmanager/ui/tasks/TaskAdapter.java
+++ app/src/main/java/com/example/taskmanager/ui/tasks/TaskAdapter.java
@@ -10,6 +10,12 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import java.util.List;
+
 import com.example.taskmanager.R;
 
 import java.util.List;
@@ -58,7 +64,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
         return taskList.size();
     }
 
-    static class TaskViewHolder extends RecyclerView.ViewHolder {
+    public class TaskViewHolder extends RecyclerView.ViewHolder {
         private TextView textTaskTitle;
         private TextView textTaskDescription;
         private TextView textDueDate;
@@ -70,7 +76,18 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
             textTaskDescription = itemView.findViewById(R.id.text_task_description);
             textDueDate = itemView.findViewById(R.id.text_due_date);
             imagePriority = itemView.findViewById(R.id.image_priority);
+
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int position = getAdapterPosition();
+                    showDeleteDialog(position);
+                    return true;
+                }
+            });
         }
+
 
         void bind(Task task) {
             textTaskTitle.setText(task.getTitle());
@@ -78,5 +95,22 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
             textDueDate.setText(task.getDueDate());
             imagePriority.setImageResource(task.getPriorityIcon());
         }
+
+        private void showDeleteDialog(final int position) {
+            // consider BottomSheetDialog for multiple options
+            AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getContext());
+            builder.setMessage("Are you sure you want to delete this task?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            taskList.remove(position);
+                            notifyItemRemoved(position);
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
     }
+
+
 }
